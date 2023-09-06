b31e8126c38c4309f0c742e59f86c28a46fb54a0
Min ho Kim <minho42@gmail.com>
2023-09-06 11:11:53 +1000

    add defaultCutoff, autoFocus

--- app/page.js
+++ app/page.js
@@ -5,26 +5,30 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
 const DDimer = () => {
-  const [cutoff, setCutoff] = useState(0.5);
+  const defaultCutoff = 0.5;
+  const [cutoff, setCutoff] = useState(defaultCutoff);
   const ageRef = useRef(null);
 
   const handleInputChange = () => {
     const newAge = ageRef.current.value;
-    if (!newAge || newAge <= 50) return;
+    if (!newAge || newAge <= 50) {
+      setCutoff(defaultCutoff);
+      return;
+    }
 
     setCutoff(newAge / 100);
   };
 
-  useEffect(() => {
-    ageRef.current.focus();
-  }, []);
+  // useEffect(() => {
+  //   ageRef.current.focus();
+  // }, []);
 
   return (
     <div className="flex flex-col gap-3 border border-zinc-300 p-6 rounded-xl">
       <h1 className="font-bold">Age-adjusted D-dimer FEU</h1>
       <div className="flex items-center gap-3">
         <Label htmlFor="age">Age</Label>
-        <Input onChange={handleInputChange} ref={ageRef} id="age" type="number" />
+        <Input autoFocus onChange={handleInputChange} ref={ageRef} id="age" type="number" />
       </div>
       <div>
         Normal: &lt;= <span className="bg-blue-100 rounded p-1 font-mono">{cutoff}</span> mg/L or
